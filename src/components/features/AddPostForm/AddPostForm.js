a3c0100d1fe8d2a23b53643ad630a4ba6ed1d879
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-05-13 12:58:57 +0200

    Add PostAdd logic

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -2,37 +2,65 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPost } from '../../../redux/postsRedux';
+import { useNavigate } from "react-router-dom";
 
 const AddPostForm = () => {
 
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published, setPublished] = useState('');
+  const [shortDescription, setShortDescription] = useState('');
+  const [content, setContent] = useState('');
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // add post
+    dispatch(addPost({ title, author, published, shortDescription, content }));
+    // clear form
+    setTitle('');
+    setAuthor('');
+    setPublished('');
+    setShortDescription('');
+    setContent('');
+    // navigate
+    navigate('/');
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col xs="12" lg="6">
           <Form.Group className="mb-3">
             <Form.Label>Title</Form.Label>
-            <Form.Control placeholder="Enter title" />
+            <Form.Control placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Author</Form.Label>
-            <Form.Control placeholder="Enter author" />
+            <Form.Control placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Published</Form.Label>
-            <Form.Control placeholder="Enter date in format DD-MM-YYYY" />
+            <Form.Control placeholder="Enter date in format DD-MM-YYYY" value={published} onChange={e => setPublished(e.target.value)} />
           </Form.Group>
         </Col>
         <Col xs="12">
           <Form.Group className="mb-3">
             <Form.Label>Short description</Form.Label>
-            <Form.Control as="textarea" rows={4} placeholder="Enter short description here" />
+            <Form.Control as="textarea" rows={4} placeholder="Enter short description here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Main content</Form.Label>
-            <Form.Control as="textarea" rows={20} placeholder="Enter main content here" />
+            <Form.Control as="textarea" rows={20} placeholder="Enter main content here" value={content} onChange={e => setContent(e.target.value)} />
           </Form.Group>
         </Col>
       </Row>
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid'
+
 //selectors
 export const getAllPosts = state => state.posts;
 export const getPostById = ({posts}, postId) => posts.find(post => post.id === postId);
@@ -5,15 +7,19 @@ export const getPostById = ({posts}, postId) => posts.find(post => post.id === p
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const REMOVE_POST = createActionName('REMOVE_POST');
+const ADD_POST = createActionName('ADD_POST');
 
 // action creators
 export const removePost = payload => ({ type: REMOVE_POST, payload });
+export const addPost = payload => ({ type: ADD_POST, newPost: { ...payload }});
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case REMOVE_POST:
       return [...statePart.filter((post) => post.id !== action.payload)];
+    case ADD_POST:
+      return [...statePart, { ...action.newPost, id: nanoid() }];
     default:
       return statePart;
   };
